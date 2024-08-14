a17b7ac400ddb41d532058fabd7271628527c29d
narcisolobo <narcisolobo@gmail.com>
2024-08-14 09:10:55 -0700

    w8d3 morning

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <section id="home">
       <Title order={1}>WOS - Fathom Recordings</Title>
-      <Text fs="italic">Last updated: Monday, August 12 (afternoon)</Text>
+      <Text fs="italic">Last updated: Wednesday, August 14 (morning)</Text>
     </section>
   )
 }
