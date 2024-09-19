fc61819793db2ec28439aceb8b288108780386af
narcisolobo <narcisolobo@gmail.com>
2024-09-19 08:12:33 -0700

    w13d4 morning

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <section id="home">
       <Title order={1}>WOS - Fathom Recordings</Title>
-      <Text fs="italic">Last updated: Thursday, September 12 (afternoon)</Text>
+      <Text fs="italic">Last updated: Thursday, September 19 (morning)</Text>
     </section>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,6 +46,7 @@ function Navbar() {
         <WeekLink href="/weeks/10" label="Week 10" />
         <WeekLink href="/weeks/11" label="Week 11" />
         <WeekLink href="/weeks/12" label="Week 12" />
+        <WeekLink href="/weeks/13" label="Week 13" />
       </NavLink>
     </AppShellNavbar>
   )
